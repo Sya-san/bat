d7b258f4ef43b15f490bea30b56235b126242eef
Sya-san <dahongzhanchi@gmail.com>
2016-12-01 20:43:34 +0800

    Create sern_terminal.bat

---
+++ sern_terminal.bat
@@ -0,0 +1,80 @@
+@echo off
+echo			SSSSSSSEEEEEEERRRRRRRNNNNNNNN
+echo			SS   SSE     E     RR NNNNN N
+echo			S SSS SE EEEEE RRRR R  NNNN N
+echo			SS SSSSE EEEEE     RR N NNN N
+echo			SSS SSSE     E R RRRR NN NN N
+echo			SSSS SSE EEEEE RR RRR NNN N N
+echo			S SSS SE EEEEE RRR RR NNNN  N
+echo			SS   SSE     E RRRR R NNNNN N
+echo			SSSSSSSEEEEEEERRRRRRRNNNNNNNN
+echo.
+echo 			TERMINAL LOGIN
+
+echo.
+set /p a=Username: <nul
+ping -n 1 127.1>nul
+set /p a=S<nul
+ping -n 1 127.1>nul
+set /p a=t<nul
+ping -n 1 127.1>nul
+set /p a=e<nul
+ping -n 1 127.1>nul
+set /p a=i<nul
+ping -n 1 127.1>nul
+set /p a=n<nul
+ping -n 1 127.1>nul
+set /p a=s <nul
+ping -n 1 127.1>nul
+
+ping -n 1 127.1>nul
+set /p a=G<nul
+ping -n 1 127.1>nul
+set /p a=a<nul
+ping -n 1 127.1>nul
+set /p a=t<nul
+ping -n 1 127.1>nul
+set /p a=e<nul
+ping -n 1 127.1>nul
+echo.
+ping -n 2 127.1>nul
+set /p a=Password: <nul
+ping -n 1 127.1>nul
+set /p a=* <nul
+ping -n 1 127.1>nul
+set /p a=* <nul
+ping -n 1 127.1>nul
+set /p a=* <nul
+ping -n 1 127.1>nul
+set /p a=* <nul
+ping -n 1 127.1>nul
+set /p a=* <nul
+ping -n 1 127.1>nul
+set /p a=* <nul
+ping -n 1 127.1>nul
+echo.
+ping -n 2 127.1>nul
+echo.
+echo Authenticated Users
+echo.
+ping -n 2 127.1>nul
+echo sern^>
+set /p a=sern^> <nul
+set /p a=r<nul
+ping -n 1 127.1>nul
+set /p a=e<nul
+ping -n 1 127.1>nul
+set /p a=p<nul
+ping -n 1 127.1>nul
+set /p a=o<nul
+ping -n 1 127.1>nul
+set /p a=r<nul
+ping -n 1 127.1>nul
+set /p a=t<nul
+ping -n 1 127.1>nul
+echo.
+ping -n 2 127.1>nul
+echo Please wait...
+
+ping 127.1 >nul
+REM pause
